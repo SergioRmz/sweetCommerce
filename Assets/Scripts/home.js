4c705e0d88c3c6fac7aaf00b28927e59b3bfa39a
Raven <glenin.aquino@gmail.com>
2023-03-30 13:44:29 -0600

    Se agrega el footer a la pagina de home ya con los cambios de daniella

--- Assets/Scripts/home.js
+++ Assets/Scripts/home.js
@@ -1,5 +1,6 @@
 $(function(){
     $("#navBarSection").load("/Assets/Components/NavBar/NavBar.html");
+    $("#footer-section").load("/Assets/Components/Footer/footer.html");
     $("#includeCategoriasPopulares").load("/Assets/Components/HomeComponets/categoriasPopularesHome.html");
  });
 
