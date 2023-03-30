bcd3bb23c36bf54e5c6776d9b2281fd651cf3c64
Daniella <dansmc.courses@gmail.com>
2023-03-30 13:08:15 -0600

    Intento 1 de corregir funcionamientos de carruseles

--- Assets/Scripts/home.js
+++ Assets/Scripts/home.js
@@ -1,8 +1,5 @@
 $(function(){
     $("#navBarSection").load("/Assets/Components/NavBar/NavBar.html");
     $("#includeCategoriasPopulares").load("/Assets/Components/HomeComponets/categoriasPopularesHome.html");
-    $("#includeProductosPopularesHome").load("/Assets/Components/HomeComponets/productosPopularesHome.html");
-    $("#includeProductosPopularesHome2").load("/Assets/Components/HomeComponets/productosPopularesHome2.html");
-
-});
+ });
 
