908d832e39598b65f06d1f58b6132f6c837523fb
Sergio Ramirez <sergio.ramirez.mtz@gmail.com>
2023-04-02 12:02:19 -0600

    cambios en la lista de deseos

--- Assets/Scripts/listaDeseos.js
+++ Assets/Scripts/listaDeseos.js
@@ -12,7 +12,7 @@ function agregarProducto(producto) {
   imagenEliminar.alt = 'boton-eliminar';
   imagenEliminar.classList.add('remove-button');
   celdaEliminar.appendChild(imagenEliminar);
-  // fila.appendChild(celdaEliminar);
+  fila.appendChild(celdaEliminar);
   
 
   // Imagen del producto
@@ -23,22 +23,22 @@ function agregarProducto(producto) {
   imagenProducto.srcset = '';
   imagenProducto.classList.add('wishlist-img');
   celdaImagen.appendChild(imagenProducto);
-  // fila.appendChild(celdaImagen);
+  fila.appendChild(celdaImagen);
 
   // Nombre del producto
   const celdaTitulo = document.createElement('td');
   celdaTitulo.textContent = producto.title;
-  // fila.appendChild(celdaTitulo);
+  fila.appendChild(celdaTitulo);
 
   // Precio del producto
   const celdaPrecio = document.createElement('td');
   celdaPrecio.textContent = producto.price;
-  // fila.appendChild(celdaPrecio);
+  fila.appendChild(celdaPrecio);
 
   // Estatus en almacen
   const celdaStatus = document.createElement('td');
   celdaStatus.textContent = 0;
-  // fila.appendChild(celdaStatus);
+  fila.appendChild(celdaStatus);
 
   // Boton añadir carrito
   const celdaBoton = document.createElement('td');
@@ -48,29 +48,21 @@ function agregarProducto(producto) {
   boton.classList.add('btn');
   celdaBoton.appendChild(boton);
 
-  // fila.appendChild(celdaBoton);
+  fila.appendChild(celdaBoton);
 
   // Agregar la fila creada previamente a la tabla de la lista de deseos
 
-  // tablaBody.appendChild(fila);
-
   const filaCompleta = {
-    "celdaEliminar": celdaEliminar,
-    "celdaImagen" : celdaImagen,
-    "celdaTitulo" : celdaTitulo,
-    "celdaPrecio": celdaPrecio,
-    "celdaBoton": celdaBoton
+    'fila' : fila,
   };
 
-
-  // localStorage.setItem("elemento2", JSON.stringify(filaCompleta));
-
+  const filaCompletaJSON = JSON.stringify(filaCompleta);
+  localStorage.setItem('filaCompleta', filaCompletaJSON);
 };
 
 // Obtener el botón con la clase "btn-fav"
 const btnFav = document.querySelector('.btn-fav');
 
-// Al hacer clic en el boton de favoritos se dispara la funcion.
 btnFav.addEventListener('click', (event) => {
   // Previene que el enlace predeterminado se abra
   event.preventDefault();
@@ -79,19 +71,17 @@ btnFav.addEventListener('click', (event) => {
   const title = document.querySelector('.card-title1').textContent;
   const price = document.querySelector('.card-text1').textContent;  
 
-  // Creacion de un objeto con el título y el precio para pasarlo a la funcion agregarProducto.
   const product = {
     title,
     price
   };
-
-  // Para efectos de debbuging
-  // console.log(product);
-
   // Agregar el producto a la lista de deseos
   agregarProducto(product);
 
 });
+
+
+
 
 
 
