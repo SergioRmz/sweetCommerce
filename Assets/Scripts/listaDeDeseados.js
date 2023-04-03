349109988fd585c70164c0ce327a4baa2f32c7f1
Daniella <dansmc.courses@gmail.com>
2023-04-02 23:23:52 -0600

    Agregando funciones a lista de deseos boton

--- Assets/Scripts/listaDeDeseados.js
+++ Assets/Scripts/listaDeDeseados.js
@@ -103,7 +103,7 @@ btnFav.addEventListener('click', (event) => {
 const productosDeseadosArrayStr = localStorage.getItem("productosDeseadosArray");
 
 /**Para convertir el json en el objeto original */
-const parsedproductosDeseadosArray = JSON.parse(productosDeseadosArrayStr);
+let parsedproductosDeseadosArray = JSON.parse(productosDeseadosArrayStr);
 
 
 
@@ -118,9 +118,9 @@ const createListaDeseados = () => {
 
         listaDeseadosHTML =
             listaDeseadosHTML +
-            '<tr>' +
+            `<tr id="${element.id}_deseadoLista">` +
             '<td>' +
-            '<img class="remove-button" src="../Assets/images/Icons/icons8-eliminar-30.png" alt="boton-eliminar">' +
+            `<a id="${element.id}_deseadoEliminar" onclick="eliminarDeseado(this)"  href="#" class="btn"><img class="remove-button" src="../Assets/images/Icons/icons8-eliminar-30.png" alt="boton-eliminar"></a>` +
             '</td>' +
             '<td>' +
             `<img class="wishlist-img" src="${element.imagenSrc}" alt="">` +
@@ -154,26 +154,45 @@ const guardarCarrito = (objetoHTML) => {
 
     console.log(objetoHTML.id.substring(0, objetoHTML.id.indexOf("_")));
 
-  /**Se obtiete el arrglo del carrito en localstorage  */
-  const productosCarritoArrayStr = localStorage.getItem("productosCarritoArray");
-  const parsedproductosCarritoArray = JSON.parse(productosCarritoArrayStr);
-/*Recorrede el arrglo del productos que se recupero del localstorage  */
-parsedproductosDeseadosArray.forEach(element => {
-    /*se comparan los ids de los productos del arrglo con el id que se obtuvodel elemento html que recibio el evento,
-     el cual se obtiene al utilizar la funcion substring del indice 0 del string del id del elemento hasta el indice 
-     en el que se encuentra un guion bajo
-     */
-    if (element.id == parseInt(objetoHTML.id.substring(0, objetoHTML.id.indexOf("_")))) {
-      /**Una vez encontrado el producto con el ID correspondiente se agrega al arreglo del carrito */
-      parsedproductosCarritoArray.push(element);
-      console.log("El elegido carrito", element);
-    }
-  });
-/**Se toma el arreglo creado con el producto que se acaba de ingresar y se vuelve a guardar en el localStorage */
-  const productosCarritoArrayJson = JSON.stringify(parsedproductosCarritoArray);
-  localStorage.setItem("productosCarritoArray", productosCarritoArrayJson);
+    const id = objetoHTML.id.substring(0, objetoHTML.id.indexOf("_"));
+    /**Se obtiete el arrglo del carrito en localstorage  */
+    const productosCarritoArrayStr = localStorage.getItem("productosCarritoArray");
+    const parsedproductosCarritoArray = JSON.parse(productosCarritoArrayStr);
+    /*Recorrede el arrglo del productos que se recupero del localstorage  */
+    parsedproductosDeseadosArray.forEach(element => {
+        /*se comparan los ids de los productos del arrglo con el id que se obtuvodel elemento html que recibio el evento,
+         el cual se obtiene al utilizar la funcion substring del indice 0 del string del id del elemento hasta el indice 
+         en el que se encuentra un guion bajo
+         */
+        if (element.id == parseInt(id)) {
+            /**Una vez encontrado el producto con el ID correspondiente se agrega al arreglo del carrito */
+            parsedproductosCarritoArray.push(element);
+            console.log("El elegido carrito", element);
+        }
+    });
+    /**Se toma el arreglo creado con el producto que se acaba de ingresar y se vuelve a guardar en el localStorage */
+    const productosCarritoArrayJson = JSON.stringify(parsedproductosCarritoArray);
+    localStorage.setItem("productosCarritoArray", productosCarritoArrayJson);
 
-  const productosCarritoArrayStr2 = localStorage.getItem("productosCarritoArray");
-  const parsedproductosCarritoArray2 = JSON.parse(productosCarritoArrayStr2);
-  console.log("LocalStorage Carrito", parsedproductosCarritoArray2);
-  }
+    const productosCarritoArrayStr2 = localStorage.getItem("productosCarritoArray");
+    const parsedproductosCarritoArray2 = JSON.parse(productosCarritoArrayStr2);
+    console.log("LocalStorage Carrito", parsedproductosCarritoArray2);
+}
+
+
+const eliminarDeseado = (objetoHTML) => {
+    console.log(objetoHTML.id.substring(0, objetoHTML.id.indexOf("_")));
+    const id = objetoHTML.id.substring(0, objetoHTML.id.indexOf("_"));
+
+    parsedproductosDeseadosArray.forEach(element => {
+        if (element.id == parseInt(id)) {
+            const index = parsedproductosDeseadosArray.indexOf(element);parsedproductosDeseadosArray = parsedproductosDeseadosArray.filter(e => parsedproductosDeseadosArray.indexOf(e) != index);
+            document.getElementById(`${id}_deseadoLista`).remove();
+        }
+    });
+
+    const productosDeseadosArrayJson = JSON.stringify(parsedproductosDeseadosArray);
+    localStorage.setItem("productosDeseadosArray", productosDeseadosArrayJson);
+
+    console.log(JSON.parse(localStorage.getItem("productosDeseadosArray")));
+}
